fc1c082670d672ddcb0dafc56fc485b314424def
Thaís Carvalho <thaiscarvalho115@gmail.com>
2024-05-27 16:34:36 -0300

    Algoritmo InsertionSort

---
+++ insertion.py
@@ -0,0 +1,45 @@
+# Function to do insertion sort
+def insertionSort(arr):
+
+    # Traverse through 1 to len(arr)
+    for i in range(1, len(arr)):
+
+        key = arr[i]
+
+        # Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
+        j = i-1
+        while j >= 0 and key < arr[j] :
+                arr[j + 1] = arr[j]
+                j -= 1
+        arr[j + 1] = key
+
+
+
+
+def read_input_file(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        arr = [int(line.strip()) for line in lines]
+    return arr
+
+def write_output_file(file_path, arr):
+    with open(file_path, 'w') as file:
+        for num in arr:
+            file.write(f"{num}\n")
+
+input_file = "/uploads/input.txt"
+output_file = "/myfiles/output.txt"
+
+
+
+
+# Read input from file
+arr = read_input_file(input_file)
+
+# Sort the array
+insertionSort(arr)
+
+# Write sorted array to file
+write_output_file(output_file, arr)
