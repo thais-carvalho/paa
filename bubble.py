5b95bc077489b69ce5fbfd92d613d30f75a04df1
Thaís Carvalho <thaiscarvalho115@gmail.com>
2024-05-27 16:29:38 -0300

    Algoritmo BubbleSort

---
+++ bubble.py
@@ -0,0 +1,47 @@
+def bubbleSort(arr):
+    n = len(arr)
+    
+    # Traverse through all array elements
+    for i in range(n):
+        swapped = False
+
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+
+            # Traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if swapped == False:
+            break
+
+
+
+
+def read_input_file(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        arr = [int(line.strip()) for line in lines]
+    return arr
+
+def write_output_file(file_path, arr):
+    with open(file_path, 'w') as file:
+        for num in arr:
+            file.write(f"{num}\n")
+
+input_file = "/uploads/input.txt"
+output_file = "/myfiles/output.txt"
+
+
+
+
+# Read input from file
+arr = read_input_file(input_file)
+
+# Sort the array
+bubbleSort(arr)
+
+# Write sorted array to file
+write_output_file(output_file, arr)
