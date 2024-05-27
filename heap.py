8227afcc6e476a5573ffa212828c50ed3f20b5ae
Thaís Carvalho <thaiscarvalho115@gmail.com>
2024-05-27 16:45:11 -0300

    Algoritmo HeapSort

---
+++ heap.py
@@ -0,0 +1,67 @@
+def heapify(arr, N, i):
+    largest = i  # Initialize largest as root
+    l = 2 * i + 1     # left = 2*i + 1
+    r = 2 * i + 2     # right = 2*i + 2
+
+    # See if left child of root exists and is
+    # greater than root
+    if l < N and arr[largest] < arr[l]:
+        largest = l
+
+    # See if right child of root exists and is
+    # greater than root
+    if r < N and arr[largest] < arr[r]:
+        largest = r
+
+    # Change root, if needed
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]  # swap
+
+        # Heapify the root.
+        heapify(arr, N, largest)
+
+# The main function to sort an array of given size
+
+
+def heapSort(arr):
+    N = len(arr)
+
+    # Build a maxheap.
+    for i in range(N//2 - 1, -1, -1):
+        heapify(arr, N, i)
+
+    # One by one extract elements
+    for i in range(N-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]  # swap
+        heapify(arr, i, 0)
+
+
+
+
+
+
+def read_input_file(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        arr = [int(line.strip()) for line in lines]
+    return arr
+
+def write_output_file(file_path, arr):
+    with open(file_path, 'w') as file:
+        for num in arr:
+            file.write(f"{num}\n")
+
+input_file = "/uploads/input.txt"
+output_file = "/myfiles/output.txt"
+
+
+
+
+# Read input from file
+arr = read_input_file(input_file)
+
+# Sort the array
+heapSort(arr)
+
+# Write sorted array to file
+write_output_file(output_file, arr)
